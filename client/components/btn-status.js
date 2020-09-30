eafbc80768dfe01940b0842022e53dd5a23f5d15
Кирилюк <kk87.work@gmail.com>
2020-09-30 18:24:07 +0300

    add console.log btn status

--- client/components/btn-status.js
+++ client/components/btn-status.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-const BtnStatus = ({ status }) => {
+const BtnStatus = ({ status, index }) => {
   return (
     <button
       type="button"
       className="mx-2 px-3 py-1 bg-gray-800 rounded-md text-sm font-medium leading-5 text-white"
+      onClick={() => console.log(index)}
     >
       {' '}
       {status}{' '}
--- client/components/task.js
+++ client/components/task.js
@@ -3,13 +3,13 @@ import BtnStatus from './btn-status'
 // import BtnEdit from './btn-edit'
 
 const Task = ({ allTasks }) => {
-  return allTasks.map((item) => {
+  return allTasks.map((item, index) => {
     return (
       <div className="flex justify-center my-2 px-2" key={item.taskId}>
         <div className="px-3 py-1 w-1/5 bg-gray-100 border-solid border-2 border-gray-800 rounded-md text-sm font-medium leading-5 text-grey-800">
           {item.title}
         </div>
-        <BtnStatus status={item.status} />
+        <BtnStatus status={item.status} index={index} />
       </div>
     )
   })
